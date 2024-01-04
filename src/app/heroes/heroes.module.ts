a60996c02110606b197f32f1825cb815f2459150
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-04 16:52:00 -0300

    fin sección 4

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HeroComponent } from './hero/hero.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  exports: [HeroComponent, ListComponent],
+  declarations: [HeroComponent, ListComponent],
+  providers: [],
+})
+export class HeroesModule { }
