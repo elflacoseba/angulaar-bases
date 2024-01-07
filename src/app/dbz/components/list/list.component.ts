c650bcc2d57599570a4408254c328c103908a29a
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-07 18:47:12 -0300

    Agrego y quito personajes.

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
+
 export class ListComponent {
 
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDeleteCharacter: EventEmitter<number> = new EventEmitter();
+
+    emitIndex(index:number):void {
+      this.onDeleteCharacter.emit(index);
+    }
 }
